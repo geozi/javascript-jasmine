7e59d463ed1bfdafee0346fd3792703fd913cda8
geozi <yorgosaz@gmail.com>
2024-10-21 20:33:03 +0300

    updated test cases

--- __tests__/checkForAnagramValidity.spec.js
+++ __tests__/checkForAnagramValidity.spec.js
@@ -1,75 +1,75 @@
-const anagram = require("../src/checkForAnagramValidity");
+const isAnagram = require("../src/checkForAnagramValidity");
 
 describe("Testing with the following inputs: ", () => {
   it("character only strings -> returns true", () => {
     let s1 = "cat";
     let s2 = "act";
-    expect(anagram(s1, s2)).toBe(true);
+    expect(isAnagram(s1, s2)).toBe(true);
   });
 
   it("number only strings -> returns true", () => {
     let s1 = "012345";
     let s2 = "513240";
-    expect(anagram(s1, s2)).toBe(true);
+    expect(isAnagram(s1, s2)).toBe(true);
   });
 
   it("mixed strings -> returns true", () => {
     let s1 = "random12";
     let s2 = "12random";
-    expect(anagram(s1, s2)).toBe(true);
+    expect(isAnagram(s1, s2)).toBe(true);
   });
 
   it("white spaces only -> returns true", () => {
     let s1 = "   ";
     let s2 = "   ";
-    expect(anagram(s1, s2)).toBe(true);
+    expect(isAnagram(s1, s2)).toBe(true);
   });
 
   it("empty strings -> returns true", () => {
     let s1 = "";
     let s2 = "";
-    expect(anagram(s1, s2)).toBe(true);
+    expect(isAnagram(s1, s2)).toBe(true);
   });
 
   it("character only strings -> returns false", () => {
     let s1 = "hello";
     let s2 = "world";
-    expect(anagram(s1, s2)).toBe(false);
+    expect(isAnagram(s1, s2)).toBe(false);
   });
 
   it("number only strings -> returns false", () => {
     let s1 = "012345";
     let s2 = "012346";
-    expect(anagram(s1, s2)).toBe(false);
+    expect(isAnagram(s1, s2)).toBe(false);
   });
 
   it("mixed strings -> returns false", () => {
     let s1 = "random12";
     let s2 = "random19";
-    expect(anagram(s1, s2)).toBe(false);
+    expect(isAnagram(s1, s2)).toBe(false);
   });
 
   it("strings with different lengths -> returns false", () => {
     let s1 = "orange";
     let s2 = "oranges";
-    expect(anagram(s1, s2)).toBe(false);
+    expect(isAnagram(s1, s2)).toBe(false);
   });
 
   it("first string null -> throws TypeError", () => {
     let s1 = null;
     let s2 = "orange";
-    expect(() => anagram(s1, s2).toThrow(TypeError));
+    expect(() => isAnagram(s1, s2).toThrow(TypeError));
   });
 
   it("second string null - throws TypeError", () => {
     let s1 = "hello";
     let s2 = null;
-    expect(() => anagram(s1, s2).toThrow(TypeError));
+    expect(() => isAnagram(s1, s2).toThrow(TypeError));
   });
 
   it("both strings null -> throws TypeError", () => {
     let s1 = null;
     let s2 = null;
-    expect(() => anagram(s1, s2).toThrow(TypeError));
+    expect(() => isAnagram(s1, s2).toThrow(TypeError));
   });
 });
--- __tests__/checkStringIsomorphy.spec.js
+++ __tests__/checkStringIsomorphy.spec.js
@@ -1,75 +1,75 @@
-const isomorphy = require("../src/checkStringIsomorphy");
+const isIsomorphic = require("../src/checkStringIsomorphy");
 
 describe("Input: ", () => {
   it("character only strings -> returns true", () => {
     let s1 = "egg";
     let s2 = "add";
-    expect(isomorphy(s1, s2)).toBe(true);
+    expect(isIsomorphic(s1, s2)).toBe(true);
   });
 
   it("character only strings -> returns false", () => {
     let s1 = "hello";
     let s2 = "world";
-    expect(isomorphy(s1, s2)).toBe(false);
+    expect(isIsomorphic(s1, s2)).toBe(false);
   });
 
   it("number only strings -> returns true", () => {
     let s1 = "01234";
     let s2 = "03654";
-    expect(isomorphy(s1, s2)).toBe(true);
+    expect(isIsomorphic(s1, s2)).toBe(true);
   });
 
   it("number only strings -> returns false", () => {
     let s1 = "345613";
     let s2 = "012345";
-    expect(isomorphy(s1, s2)).toBe(false);
+    expect(isIsomorphic(s1, s2)).toBe(false);
   });
 
   it("mixed character strings -> returns true", () => {
     let s1 = "random12";
     let s2 = "mixed130";
-    expect(isomorphy(s1, s2)).toBe(true);
+    expect(isIsomorphic(s1, s2)).toBe(true);
   });
 
   it("mixed character strings -> returns false", () => {
     let s1 = "random12";
     let s2 = "mixed131";
-    expect(isomorphy(s1, s2)).toBe(false);
+    expect(isIsomorphic(s1, s2)).toBe(false);
   });
 
   it("white spaces only strings -> returns true", () => {
     let s1 = "   ";
     let s2 = "   ";
-    expect(isomorphy(s1, s2)).toBe(true);
+    expect(isIsomorphic(s1, s2)).toBe(true);
   });
 
   it("empty strings -> return true", () => {
     let s1 = "";
     let s2 = "";
-    expect(isomorphy(s1, s2)).toBe(true);
+    expect(isIsomorphic(s1, s2)).toBe(true);
   });
 
   it("strings with different lengths -> returns false", () => {
     let s1 = "apple";
     let s2 = "stories";
-    expect(isomorphy(s1, s2)).toBe(false);
+    expect(isIsomorphic(s1, s2)).toBe(false);
   });
 
   it("first string null -> throws TypeError", () => {
     let s1 = null;
     let s2 = "weather";
-    expect(() => isomorphy(s1, s2).toThrow(TypeError));
+    expect(() => isIsomorphic(s1, s2).toThrow(TypeError));
   });
 
   it("second string null -> throws TypeError", () => {
     let s1 = "sun";
     let s2 = null;
-    expect(() => isomorphy(s1, s2).toThrow(TypeError));
+    expect(() => isIsomorphic(s1, s2).toThrow(TypeError));
   });
 
   it("null strings -> throws TypeError", () => {
     let s1 = null;
     let s2 = null;
-    expect(() => isomorphy(s1, s2).toThrow(TypeError));
+    expect(() => isIsomorphic(s1, s2).toThrow(TypeError));
   });
 });
